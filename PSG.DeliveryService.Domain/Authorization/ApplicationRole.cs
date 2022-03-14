023f0c9e903c058aec6190cad2a5627567333131
dotnetmaslenok <a.kudryavcev0812@bk.ru>
2022-03-14 22:33:06 +0300

    initial commit

---
+++ PSG.DeliveryService.Domain/Authorization/ApplicationRole.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace PSG.DeliveryService.Domain.Authorization
+{
+	public class ApplicationRole : IdentityRole<int>
+	{
+
+	}
+}
---
+++ PSG.DeliveryService.Domain/Entities/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace PSG.DeliveryService.Domain.Entities
+{
+	public class ApplicationUser : IdentityUser<int>
+	{
+		public DateTime UserRegistrationTime { get; set; }
+
+		public string PreferredAdress { get; set; }
+
+		public List<Order> Orders { get; set; }
+	}
+}
