94dfca96814a246633d470f9734b2fcd3723768e
dotnetmaslenok <84874724+dotnetmaslenok@users.noreply.github.com>
2022-04-18 00:01:16 +0300

    Delete waitConnection.sh

--- waitConnection.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-pg_isready -h ds_postgres -p 5432 -U ds_postgres
-if [ $state -ne "0" ]; then
-    echo "Waiting for postgres";
-else
-    echo $state;
-    echo "Succeeded";
-    sleep 5;
-fi
